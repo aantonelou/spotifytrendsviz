13b9400f0aeeb7f90f8d2b794140118b07995eb0
Anastasia Antonelou <an.antonelou@gmail.com>
2024-07-06 23:50:24 +0300

    Updated text

--- src/components/Sections/InsightsText2023.tsx
+++ src/components/Sections/InsightsText2023.tsx
@@ -2,19 +2,18 @@ import { FC } from 'react';
 
 const InsightsText2023: FC = () => (
   <div style={{ padding: '1rem', borderRadius: '8px', margin: '2rem 0', color: 'white' }}>
-    <p>The "<strong>Top 20 Tracks of 2023 by Streams</strong>" dashboard showcases a wide range of tracks from different years. The
-    dominance of older tracks in the top 20 indicates the power of streaming platforms in keeping music relevant over the years.</p>
+    <p>The "<strong>Top 20 Tracks of 2023 by Streams</strong>" dashboard showcases a fascinating mix of songs from various years. 
+    The prevalence of older tracks in the top 20 underscores the power of streaming platforms in maintaining the relevance of music over time.</p>
     <br/>
-    <p>The blend of artists in the "<strong>Top 20 Artists of 2023</strong>" dashboard shows the dynamic nature of the music industry, 
-    where both established and relatively newer names appear, also representing a variety of music genres.</p>
+    <p>The "<strong>Top 20 Artists of 2023</strong>" dashboard illustrates the dynamic nature of the music industry, 
+    featuring both established and newer artists across a variety of genres.</p>
     <br/>
-    <p>The "<strong>Top 20 Tracks of 2023 in Spotify Playlists</strong>" dashboard also indicates that both old and new tracks are favored, 
-    showing a blend of nostalgia and contemporary tastes in playlists.</p>
+    <p>Similarly, the "<strong>Top 20 Tracks of 2023 in Spotify Playlists</strong>" dashboard indicates a 
+    preference for both old and new tracks, reflecting a blend of nostalgia and contemporary tastes in playlists.</p>
     <br/>
-    <p>All these factors indicate not only the timeless quality and appeal of tracks released several years ago
-     but also the role of digital platforms in preserving and promoting these hits across generations. 
-     Such tracks continue to enjoy significant streaming numbers and appear in numerous playlists, reflecting their strong connection with the audience.
-     This can be seen more analytically in the following dashboard "<strong>Track Popularity Over Time</strong>", which shows the release times of all the top tracks of 2023.</p>
+    <p>These observations highlight the timeless appeal of many older tracks and the role of digital platforms in sustaining their popularity. 
+      These songs continue to enjoy significant streaming numbers and appear in numerous playlists, demonstrating their strong connection with audiences.
+      This trend is further analyzed in the "<strong>Track Popularity Over Time</strong>" dashboard, which shows the release dates of all the top tracks of 2023.</p>
   </div>
 );
 

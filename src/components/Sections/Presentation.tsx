a9f6b5ffdf84c2ab0134b0cd6d1f75acc0c65ab0
Anastasia Antonelou <an.antonelou@gmail.com>
2024-07-10 15:06:03 +0300

    Added youtube video

--- src/components/Sections/Presentation.tsx
+++ src/components/Sections/Presentation.tsx
@@ -11,16 +11,16 @@ const Presentation: FC = memo(() => {
             <p>Here you can watch a quick overview of the website and our visualizations.</p>
           </div>
         </div>
-        {/* <div className="w-full md:w-1/2">
+        <div className="w-full md:w-1/2">
           <iframe 
             width="100%" 
             height="500" 
-            src="https://www.youtube-nocookie.com/embed/UDfGl11_P-Q?si=g7JKAbElRtpO_Nl5" 
+            src="https://www.youtube-nocookie.com/embed/_jynNdCrWUI?si=grAJ4aToiSK8M4mU" 
             title="YouTube video player" 
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
             allowFullScreen>
           </iframe>
-        </div> */}
+        </div>
       </div>
     </Section>
   );
